eeb6d0de61fe17e9c5a7869a15d66e8dde3ad74d
Bernard Mordan <bernardmordan@gmail.com>
2015-03-22 13:48:20 +0000

    Basic working example but flashing no good

--- flip.js
+++ flip.js
@@ -1,21 +1,20 @@
 (function(){
 
   var el = document.getElementById('scrollAnimator')
-  var frames = {"0":"tableflip.gif.000","1":"tableflip.gif.001","2":"tableflip.gif.002","3":"tableflip.gif.003","4":"tableflip.gif.004","5":"tableflip.gif.005","6":"tableflip.gif.006","7":"tableflip.gif.007","8":"tableflip.gif.008","9":"tableflip.gif.009","10":"tableflip.gif.010","11":"tableflip.gif.011","12":"tableflip.gif.012","13":"tableflip.gif.013","14":"tableflip.gif.014","15":"tableflip.gif.015","16":"tableflip.gif.016","17":"tableflip.gif.017","18":"tableflip.gif.018"}
+  var frames = {"0":"tableflip.gif.018","1":"tableflip.gif.017","2":"tableflip.gif.016","3":"tableflip.gif.015","4":"tableflip.gif.014","5":"tableflip.gif.013","6":"tableflip.gif.012","7":"tableflip.gif.011","8":"tableflip.gif.010","9":"tableflip.gif.009","10":"tableflip.gif.008","11":"tableflip.gif.007","12":"tableflip.gif.006","13":"tableflip.gif.005","14":"tableflip.gif.004","15":"tableflip.gif.003","16":"tableflip.gif.002","17":"tableflip.gif.001"}
 
-  el.innerHTML = '<img src="src/'+frames[0]+'">'
+  el.innerHTML = '<img src="assets/'+frames[0]+'" style="width:'+window.innerWidth+'px">'
 
   window.onscroll = function (event) { 
-    // var base = anillax.getBoundingClientRect().bottom
     var top = document.body.scrollTop
     var base = el.offsetHeight
     var chunk = base/Object.keys(frames).length
     var index = returnIndex(top, chunk)
-    
+    el.innerHTML = '<img src="assets/'+frames[index]+'" style="width:'+window.innerWidth+'px">'
   }
 
   function returnIndex (top, chunk) {
-    if (top >= anillax.offsetHeight) return Object.keys(frames).length
+    if (top >= el.offsetHeight) return Object.keys(frames).length-1
     if (top <= 0) return 0
     return Math.floor(top/chunk)
   }
